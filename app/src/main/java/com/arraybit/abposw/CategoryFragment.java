c9ee50a42d122ee595b9d53cf9c56abfdbd81da7
arraybit <arraybit>
2016-03-14 11:55:02 +0530

    abPOSW

---
+++ app/src/main/java/com/arraybit/abposw/CategoryFragment.java
@@ -0,0 +1,46 @@
+package com.arraybit.abposw;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.arraybit.global.Globals;
+import com.arraybit.modal.CategoryMaster;
+import com.arraybit.parser.ItemJSONParser;
+
+public class CategoryFragment extends Fragment {
+
+    CategoryMaster objCategoryMaster;
+    RecyclerView rvItemMaster;
+    ProgressDialog progressDialog;
+    int currentPage = 1;
+
+    public CategoryFragment(CategoryMaster objCategoryMaster) {
+        this.objCategoryMaster = objCategoryMaster;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_category, container, false);
+
+        rvItemMaster = (RecyclerView) view.findViewById(R.id.rvItemMaster);
+        rvItemMaster.setNestedScrollingEnabled(false);
+        rvItemMaster.setVisibility(View.GONE);
+
+        RequestItemMaster();
+        return view;
+    }
+
+    //region Private Methods
+    private void RequestItemMaster() {
+        progressDialog = new ProgressDialog();
+        progressDialog.show(getActivity().getSupportFragmentManager(), "");
+        ItemJSONParser objItemJSONParser = new ItemJSONParser();
+        objItemJSONParser.SelectAllItemMasterPageWise(this, getActivity(), String.valueOf(currentPage), String.valueOf(Globals.linktoBusinessMasterId));
+    }
+    //endregion
+}
