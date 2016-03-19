1ef07c101c7e284208f0d6c159b323de803745de
arraybit <arraybit>
2016-03-19 16:33:05 +0530

    abPOSW

--- app/src/main/java/com/arraybit/abposw/AddQtyRemarkDialogFragment.java
+++ app/src/main/java/com/arraybit/abposw/AddQtyRemarkDialogFragment.java
@@ -8,8 +8,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 
-public class AddQtyRemarkDialogFragment extends DialogFragment {
+import com.rey.material.widget.Button;
+import com.rey.material.widget.ImageButton;
 
+public class AddQtyRemarkDialogFragment extends DialogFragment implements View.OnClickListener {
+
+    ImageButton ibMinus, ibPlus;
+    Button btnCancel, btnOk;
 
     public AddQtyRemarkDialogFragment() {
         // Required empty public constructor
@@ -21,7 +26,31 @@ public class AddQtyRemarkDialogFragment extends DialogFragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-        return inflater.inflate(R.layout.fragment_add_qty_remark_dialog, container, false);
+        View view = inflater.inflate(R.layout.fragment_add_qty_remark_dialog, container, false);
+
+        ibMinus = (ImageButton) view.findViewById(R.id.ibMinus);
+        ibPlus = (ImageButton) view.findViewById(R.id.ibPlus);
+        btnCancel = (Button) view.findViewById(R.id.btnCancel);
+        btnOk = (Button) view.findViewById(R.id.btnAdd);
+
+        btnCancel.setOnClickListener(this);
+        btnOk.setOnClickListener(this);
+        ibMinus.setOnClickListener(this);
+        ibPlus.setOnClickListener(this);
+
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnOk) {
+
+        } else if (v.getId() == R.id.btnCancel) {
+
+        } else if (v.getId() == R.id.ibMinus) {
+
+        } else if (v.getId() == R.id.ibPlus) {
+
+        }
+    }
 }
