0eeef4192e49bc07f01ff5c84bf06476d8fe9639
arraybit <arraybit>
2016-03-21 13:33:09 +0530

    abPOSW

--- app/src/main/java/com/arraybit/abposw/DetailActivity.java
+++ app/src/main/java/com/arraybit/abposw/DetailActivity.java
@@ -37,6 +37,7 @@ public class DetailActivity extends AppCompatActivity implements ItemJSONParser.
     ItemSuggestedAdapter itemSuggestedAdapter;
     com.arraybit.abposw.ProgressDialog progressDialog = new com.arraybit.abposw.ProgressDialog();
     LinearLayout detailLinearLayout, itemSuggestionLayout, dividerLayout;
+    //test
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
