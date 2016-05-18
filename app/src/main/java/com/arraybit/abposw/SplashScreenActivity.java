42226163aa8009b216458140c4005388aaa7023c
arraybit <info@arraybit.com>
2016-05-18 18:41:22 +0530

    abPOSW

--- app/src/main/java/com/arraybit/abposw/SplashScreenActivity.java
+++ app/src/main/java/com/arraybit/abposw/SplashScreenActivity.java
@@ -1,46 +1,82 @@
 package com.arraybit.abposw;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.ThumbnailUtils;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.widget.ImageView;
 
 import com.arraybit.global.Globals;
 import com.arraybit.global.SharePreferenceManage;
 import com.arraybit.modal.CustomerMaster;
 import com.arraybit.parser.CustomerJSONParser;
+import com.squareup.picasso.Picasso;
 
 public class SplashScreenActivity extends AppCompatActivity implements CustomerJSONParser.CustomerRequestListener {
 
     SharePreferenceManage objSharePreferenceManage;
+    DisplayMetrics displayMetrics;
+    ImageView ivLeft, ivRight, ivLogo, ivText, ivSwipe;
+    DrawerLayout mainLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        mainLayout = (DrawerLayout) findViewById(R.id.mainLayout);
+
+        displayMetrics = getResources().getDisplayMetrics();
+
+        Bitmap originalBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.welcome_background);
+        Bitmap resizeBitmap = ThumbnailUtils.extractThumbnail(originalBitmap, displayMetrics.widthPixels, displayMetrics.heightPixels);
+
+        mainLayout.setBackground(new BitmapDrawable(getResources(), resizeBitmap));
+
+        ivLeft = (ImageView) findViewById(R.id.ivLeft);
+        ivRight = (ImageView) findViewById(R.id.ivRight);
+        ivLogo = (ImageView) findViewById(R.id.ivLogo);
+        ivText = (ImageView) findViewById(R.id.ivText);
+        ivSwipe = (ImageView) findViewById(R.id.ivSwipe);
+
+        Picasso.with(SplashScreenActivity.this).load(R.drawable.left_design).resize((displayMetrics.widthPixels * 50) / 100, (displayMetrics.heightPixels * 20) / 100).into(ivLeft);
+        Picasso.with(SplashScreenActivity.this).load(R.drawable.right_design).resize((displayMetrics.widthPixels * 50) / 100, (displayMetrics.heightPixels * 20) / 100).into(ivRight);
+        Picasso.with(SplashScreenActivity.this).load(R.drawable.likeat_logo).resize((displayMetrics.widthPixels * 25) / 100, (displayMetrics.heightPixels * 8) / 100).into(ivLogo);
+        Picasso.with(SplashScreenActivity.this).load(R.drawable.welcome_text).resize((displayMetrics.widthPixels * 80) / 100, (displayMetrics.heightPixels * 6) / 100).into(ivText);
+
         objSharePreferenceManage = new SharePreferenceManage();
-        if (objSharePreferenceManage.GetPreference("LoginPreference", "UserName", this) != null && objSharePreferenceManage.GetPreference("LoginPreference", "UserPassword", this) != null) {
-            String userName = objSharePreferenceManage.GetPreference("LoginPreference", "UserName", this);
-            String userPassword = objSharePreferenceManage.GetPreference("LoginPreference", "UserPassword", this);
-            if (!userName.isEmpty() && !userPassword.isEmpty()) {
-                CustomerJSONParser objCustomerJSONParser = new CustomerJSONParser();
-                objCustomerJSONParser.SelectCustomerMaster(SplashScreenActivity.this, userName, userPassword, null, null,String.valueOf(Globals.linktoBusinessMasterId));
-            } else {
-                Intent intent = new Intent(SplashScreenActivity.this, HomeActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        } else {
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    Intent intent = new Intent(SplashScreenActivity.this, HomeActivity.class);
-                    startActivity(intent);
-                    finish();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (objSharePreferenceManage.GetPreference("LoginPreference", "UserName", SplashScreenActivity.this) != null && objSharePreferenceManage.GetPreference("LoginPreference", "UserPassword", SplashScreenActivity.this) != null) {
+                    String userName = objSharePreferenceManage.GetPreference("LoginPreference", "UserName", SplashScreenActivity.this);
+                    String userPassword = objSharePreferenceManage.GetPreference("LoginPreference", "UserPassword", SplashScreenActivity.this);
+                    if (!userName.isEmpty() && !userPassword.isEmpty()) {
+                        CustomerJSONParser objCustomerJSONParser = new CustomerJSONParser();
+                        objCustomerJSONParser.SelectCustomerMaster(SplashScreenActivity.this, userName, userPassword, null, null, String.valueOf(Globals.linktoBusinessMasterId));
+                    } else {
+                        Intent intent = new Intent(SplashScreenActivity.this, HomeActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                } else {
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Intent intent = new Intent(SplashScreenActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }, 3000);
                 }
-            }, 3000);
-        }
+            }
+        }, 1000);
     }
 
     @Override
